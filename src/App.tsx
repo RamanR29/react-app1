87a13d7c74ac7e237720e5120f6edbb90437bc16
RamanR29 <r.raginya@gmail.com>
2025-09-04 15:45:57 +0300

    testing git

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="title">Raman Rahinia</div>
-      <div className="info">I like to play video games like <a href="https://www.counter-strike.net/">CS2</a> or <a href="https://playvalorant.com/en-us/">VALORANT</a></div>
+      <div className="info">I like to play video games like <a href="https://www.counter-strike.net/">CS2</a> or <a href="https://playvalorant.com/en-us/"> VALORANT</a></div>
     </div>
   );
 }
