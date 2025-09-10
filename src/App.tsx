9e6a16bc649ed2f6118bc30c69453d5d19f5cf6a
RamanR29 <r.raginya@gmail.com>
2025-09-10 13:54:32 +0300

    fixed counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
 import './App.scss';
 import Title from './components/Title';
-import { Counter } from './components/Counter/Counter';
-import { Buttons } from './components/Counter/Buttons';
-import { useState } from 'react';
-import { CounterAction } from './components/types/common';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleCount(action: CounterAction) {
-    switch (action) {
-      case 'increase':
-        setCount(count + 1);
-        break;
-      case 'decrease':
-        setCount(count - 1);
-        break;
-      case 'reset':
-        setCount(0);
-    }
-  }
   return (
     <div className="App">
-      <Counter
-        count={count}
-        changeCount={function (): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-      <Buttons changeCount={handleCount} count={count} />
+      <Counter />
       <Title title="Raman Rahinia" />
       <div className="info">
         <p>I like to play video games like</p> <a href={process.env.REACT_APP_CS_LINK}>CS2</a> or{' '}
--- src/components/Counter/Counter.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './Counter.module.scss';
-interface CounterProps {
-  count: number;
-  changeCount: () => void;
-}
-export function Counter({ count }: CounterProps) {
-  return <h2 className={styles.count}>Count: {count}</h2>;
-}
